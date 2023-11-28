e8e4c9e68b1fb1e7632cf206d81f56934ee7bc8c
LJ Patterson <ljewel@mit.edu>
2023-11-28 11:41:42 -0500

    added vuetify/mdi

--- client/main.ts
+++ client/main.ts
@@ -8,7 +8,19 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-const app = createApp(App);
+// Vuetify
+import "@mdi/font/css/materialdesignicons.css";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import "vuetify/styles";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+const app = createApp(App).use(vuetify);
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedState);
 
