b886e1211c3b20e8a6c992664e66d529d086d871
Diego Coello <dcoello@mit.edu>
2023-11-29 21:46:29 -0500

    implemented Fact component

---
+++ client/components/Fact/FactComponent.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section>
+    <!-- Add light bulb image -->
+    <p class="daily-quote-title">Daily Fact</p>
+    <!-- Change to use fact from update -->
+    <p>Glass is 100% recyclable and can be recycled endlessly without loss in quality or purity</p>
+  </section>
+</template>
+
+<style scoped>
+section {
+  max-width: 60em;
+  margin: 0.67em auto 1em;
+}
+
+.daily-quote-title {
+  font-weight: bold;
+  font-size: 3em;
+}
+</style>
