5918934622ee9f56c4435bf8c37eb3719acf7443
Diego Coello <dcoello@mit.edu>
2023-11-29 22:03:42 -0500

    adding a little more styling to Fact component

--- client/components/Fact/FactComponent.vue
+++ client/components/Fact/FactComponent.vue
@@ -3,9 +3,12 @@
 <template>
   <section>
     <!-- Add light bulb image -->
-    <p class="daily-quote-title">Daily Fact</p>
-    <!-- Change to use fact from update -->
-    <p>Glass is 100% recyclable and can be recycled endlessly without loss in quality or purity</p>
+    <img src="/client/assets/images/lightbulb.svg" />
+    <div class="fact-content">
+      <p class="fact-title">Daily Fact</p>
+      <!-- Change to use fact from update -->
+      <p class="fact">Glass is 100% recyclable and can be recycled endlessly without loss in quality or purity</p>
+    </div>
   </section>
 </template>
 
@@ -13,10 +16,25 @@
 section {
   max-width: 60em;
   margin: 0.67em auto 1em;
+  display: flex;
+  align-items: center;
+  background-color: lightgray;
+  border-radius: 0.5em;
+  gap: 1em;
 }
 
-.daily-quote-title {
+.fact-title {
   font-weight: bold;
-  font-size: 3em;
+  font-size: 2em;
+}
+
+img {
+  height: 6em;
+  padding: 1em;
+  margin-left: 1em;
+}
+
+.fact {
+  font-size: large;
 }
 </style>
