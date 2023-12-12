4ea01fa7d13406e9ebf57c40428d019d8b8696f8
LJ Patterson <ljewel@mit.edu>
2023-12-12 12:31:43 -0500

    added legend

--- client/views/MapView.vue
+++ client/views/MapView.vue
@@ -5,6 +5,24 @@ import MapComponent from "@/components/Map/MapComponent.vue";
 <template>
   <main>
     <MapComponent />
+    <section>
+      <article>
+        <v-icon style="color: green" icon="mdi-map-marker" />
+        <strong>Compost Bin</strong>
+      </article>
+      <article>
+        <v-icon style="color: blue" icon="mdi-map-marker" />
+        <strong>Recycling Bin</strong>
+      </article>
+      <article>
+        <v-icon style="color: orange" icon="mdi-map-marker" />
+        <strong>Donation Bin</strong>
+      </article>
+      <article>
+        <v-icon style="color: black" icon="mdi-map-marker" />
+        <strong>Trash Bin</strong>
+      </article>
+    </section>
   </main>
 </template>
 
@@ -14,6 +32,17 @@ main {
   flex-direction: column;
   align-items: center;
   margin: 1rem 0;
+}
+
+section {
+  display: flex;
+  flex-wrap: wrap;
   gap: 2rem;
+  margin-bottom: 1rem;
+}
+
+article {
+  display: flex;
+  gap: 0.5rem;
 }
 </style>
