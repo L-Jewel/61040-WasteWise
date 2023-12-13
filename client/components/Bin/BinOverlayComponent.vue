b9562c13dbd1f058ab4ca047e1643d3f206d8096
Dana Hua <huadana1@gmail.com>
2023-12-13 16:38:34 -0500

    add labels to materials in bin info panel

--- client/components/Bin/BinOverlayComponent.vue
+++ client/components/Bin/BinOverlayComponent.vue
@@ -109,32 +109,36 @@ onBeforeMount(async () => {
         <div>
           <h3>Accepts</h3>
           <div class="material-row" v-if="acceptedMaterials.length > 0">
-            <v-img
-              v-bind:width="IMAGE_WIDTH"
-              @click="redirectToMaterialPage(ba.name)"
-              class="bin-accepts"
-              v-for="ba in acceptedMaterials"
-              cover
-              :src="ba.image"
-              :aspect-ratio="1 / 1"
-              v-bind:key="ba._id"
-            />
+            <div class="labeled-material" v-for="ba in acceptedMaterials">
+              <v-img
+                v-bind:width="IMAGE_WIDTH"
+                @click="redirectToMaterialPage(ba.name)"
+                class="bin-accepts"
+                cover
+                :src="ba.image"
+                :aspect-ratio="1 / 1"
+                v-bind:key="ba._id"
+              />
+              <p> {{ ba.name }}</p>
+            </div>
           </div>
           <p v-else><i>Materials accepted at this bin are unknown.</i></p>
         </div>
         <div v-if="misdisposedMaterials.length > 0">
           <h3>Does not accept</h3>
           <div class="material-row">
-            <v-img
-              v-bind:width="IMAGE_WIDTH"
-              @click="redirectToMaterialPage(ba.name)"
-              class="bin-no-accepts"
-              v-for="ba in misdisposedMaterials"
-              cover
-              :src="ba.image"
-              :aspect-ratio="1 / 1"
-              v-bind:key="ba._id"
-            />
+            <div class="labeled-material" v-for="ba in misdisposedMaterials">
+              <v-img
+                v-bind:width="IMAGE_WIDTH"
+                @click="redirectToMaterialPage(ba.name)"
+                class="bin-no-accepts"
+                cover
+                :src="ba.image"
+                :aspect-ratio="1 / 1"
+                v-bind:key="ba._id"
+              />
+              <p>{{ ba.name }}</p>
+            </div>
           </div>
         </div>
         <div v-if="isLoggedIn" class="bin-btn-col">
@@ -169,18 +173,28 @@ onBeforeMount(async () => {
   overflow-x: auto;
   display: flex;
   gap: 0.5rem;
+  max-width: 100%;
+}
+
+.labeled-material {
+  width: 30%;
+  min-width: 30%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  text-align: center;
+  font-size: 0.9rem;
 }
 
 .bin-accepts {
   border: solid 0.2rem green;
-  width: 30%;
-  max-width: 30%;
+  min-width: 100%;
 }
 
 .bin-no-accepts {
   border: solid 0.2rem darkred;
-  width: 30%;
-  max-width: 30%;
+  min-width: 100%;
 }
 
 .bin-btn-col {
